d70195dee02bcfbaffdd23d209d68c429cc70ba5
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:10:42 +0500

    Task : 25

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-// Task:26 Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
-// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-// If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-// • Write one version of this program that runs the if block and another that runs the else block.
-// Create a Variale:
+// Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+//  Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 var aliens_color = "green";
-if (aliens_color == "green") {
-    console.log("player just earned 5 points for shooting the alien."); // Print Message:
-}
-else {
-    console.log("player just earned 10 points."); // Print Message:
-}
-// Declear a Variable:
+if (aliens_color == "green")
+    console.log("You are 5 points"); // Print Value
 var alien_color = "red";
-if (alien_color == "green") {
-    console.log("player just earned 5 points for shooting the alien."); // Print Message:
-}
-else {
-    console.log("player just earned 10 points."); // Print Message:
-}
+if (alien_color == "green")
+    console.log("no output"); // Print Value
--- main.ts
+++ main.ts
@@ -1,22 +1,12 @@
-// Task:26 Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
-// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-// If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-// • Write one version of this program that runs the if block and another that runs the else block.
+// Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+//  Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 
-// Create a Variale:
 let aliens_color = "green"
 
-if (aliens_color == "green") {
-console.log("player just earned 5 points for shooting the alien.") // Print Message:
-} else{
-    console.log("player just earned 10 points.") // Print Message:
-}
-// Declear a Variable:
-let alien_color = "red"
+if (aliens_color == "green")
+console.log("You are 5 points") // Print Value
 
-if (alien_color == "green") {
-console.log("player just earned 5 points for shooting the alien.") // Print Message:
-} else{
-    console.log("player just earned 10 points.") // Print Message:
-}
-
+let alien_color : string = "red"
+if (alien_color == "green")
+console.log("no output") // Print Value
