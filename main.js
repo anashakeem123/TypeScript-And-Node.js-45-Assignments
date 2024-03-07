b8ec277d08e50a69835a103393c05e5d888988a0
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:16:20 +0500

    Task : 19

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// Task:20
-// Use full Hacking Programming Languages Array
-var HackingLanguages = ["C", "java", "JAvaScript", "Ruby", "PHP", "Bash", "SQL", "Perl"];
-// Print the Message of Array
-console.log("List of Programming Languages:"); // Print Message:
-// Print the value of Array in the Form of List
-HackingLanguages.forEach(function (Hack) { return console.log(Hack); }); // Print Message List:
+// Task:19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+// Create a Array list
+var guestList = ["Anas", "Bilal", "Hamza", "Haider"]; // Create a index list:
+guestList.forEach(function (onGuest) { return console.log("Salam ".concat(onGuest, ", would you Like to dinner with me?")); }); // items list:
+var lenghtGuests = guestList.length;
+console.log("We are Inviting total ".concat(lenghtGuests, " guest.")); // Print Message:
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-// Task:20
-// Use full Hacking Programming Languages Array
-let HackingLanguages:string[]=["C","java","JAvaScript","Ruby","PHP","Bash","SQL","Perl"];
-// Print the Message of Array
-console.log("List of Programming Languages:"); // Print Message:
-// Print the value of Array in the Form of List
-HackingLanguages.forEach(Hack => console.log(Hack)); // Print Message List:
+// Task:19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+// Create a Array list
+let guestList = ["Anas","Bilal","Hamza","Haider"]; // Create a index list:
+guestList.forEach(onGuest => console.log(`Salam ${onGuest}, would you Like to dinner with me?`)); // items list:
+
+let lenghtGuests:number = guestList.length;
+console.log(`We are Inviting total ${lenghtGuests} guest.`); // Print Message:
