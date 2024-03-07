ed44c27fa58a7217a76699f73d368f645c6ab04f
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:30:04 +0500

    Task : 04

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// Task:05 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
-// Then compose your message and store it in a new variable called message. Print your message.
-//  once said, “A person who never made a mistake never tried anything new.”
-// Create a Variable:
-var famousPerson = "Albert Einstein";
-var message = "once said, “A person who never made a mistake never tried anything new.”";
-console.log(famousPerson, message); // Print Message:
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+console.log("Albert Einstein once said,\“A person who never made a mistake never tried anything new.\”"); // Print Message
