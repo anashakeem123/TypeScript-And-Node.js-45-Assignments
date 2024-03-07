aa5e532b32c423e6a9567c884209236c8cc0ab75
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:20:43 +0500

    Task : 14

--- main.ts
+++ main.ts
@@ -1,37 +1,7 @@
-// Task:15 Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-// • Print a second set of invitation messages, one for each person who is still in your list.
+// Task:14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+// Make a list that includes at least three people you’d like to invite to dinner. 
+// Then use your list to print a message to each person, inviting them to dinner.
 
-// Create a Guest List Array
-let guestList = ["Anas","Bilal","Hamza","Haider"];
-
-// Making Variable for those guest who cant come 
-let dontcome = guestList[0];
-
-// Print the name of guest who cant come 
-console.log(dontcome, "Nahi Aha Skta");
-
-// Add or Remove values from Guest List Array
-guestList.splice(0,1,"Hakeem");
- 
-// Message Print for Bigger Table
-console.log("Good News ! We have Found a Bigger Table For Dinner.");
-
-
-// Adding a new value at starting index of array
-guestList.unshift("Kamran");
-
-// Adding a New value at ending index of array
-guestList.push("Talha");
-
-//Making a variable for storing a middle index of our  guest list array
-let middleIndex:number=Math.floor(guestList.length / 2);
-
-//Adding a new guest to Middle index of array
-guestList.splice(middleIndex, 0, "Ali");
-// Print Message of Updates List
-console.log("Update List of our Guests");
-
-// Sending a invitation Message to our guests one by one with their names  
-guestList.forEach(oneguest => console.log(`Salam ${oneguest},would you like to dinner with me`));
+// Create a Variable
+let guestList = ["Anas","Bilal","Hamza","Haider"]; // Create a index value:
+guestList.forEach(onGuest => console.log(`Salam ${onGuest}, would you Like to dinner with me?`)); // Print List items
