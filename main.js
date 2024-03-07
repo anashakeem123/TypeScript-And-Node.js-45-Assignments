c6ae6724f0ca1baf90e26697091b6deae3d56d3d
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 00:55:10 +0500

    Task : 40

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-// Task:41 Make a array of magician’s names.
-// Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
-function show_magicians(magicians) {
-    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
-        var magician_1 = magicians_1[_i];
-        console.log(magician_1.charAt(0).toUpperCase() + magician_1.slice(1));
-    }
+// Task:40 Album: Write a function called make_album() that builds a Object describing a music album. 
+//  The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information.
+//  Use the function to make three dictionaries representing different albums. 
+//  Print each return value to show that Objects are storing the album information correctly. 
+// Add an optional parameter to make_album() that allows you to store the number of tracks on an album. 
+// If the calling line includes a value for the number of tracks, add that value to the album’s Object.
+// Make at least one new function call that includes the number of tracks on an album.
+function makeAlbum(artist, title) {
+    var dictionaries = {
+        artist: artist.charAt(0).toUpperCase() + artist.slice(1),
+        title: title.charAt(0).toUpperCase() + title.slice(1),
+    };
+    return dictionaries;
 }
-var magician = ["ali", "hamza", "bilal"];
-show_magicians(magician);
+var album = makeAlbum("ali", "light");
+console.log(album); // Print Message:
+album = makeAlbum("bilal", "red wave");
+console.log(album); // Print Message:
+album = makeAlbum("hamza", "seenbreez");
+console.log(album); // Print Message:
