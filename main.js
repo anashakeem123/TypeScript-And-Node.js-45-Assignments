3fe981bda2d7be10e4f37b315837d320507559f8
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:26:51 +0500

    Task : 07 && 08

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-// Task:09 Favorite Number: Store your favorite number in a variable. 
-// Then, using that variable, create a message that reveals your favorite number. Print that message.
-// Create a Variable
-var favorite_Number = 10;
-var message = "My favorite Number is:";
-console.log(message, favorite_Number); // Print Message and Number:
+// Task:7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Task:8 You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+// in Addition:
+console.log(4 + 4); // Print Value
+// in sunbtraction:
+console.log(12 - 4); // Print Value
+// in multiplication:
+console.log(4 * 2); // Print Value
+// in Division:
+console.log(16 / 2); // Print Value
--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-// Task:09 Favorite Number: Store your favorite number in a variable. 
-// Then, using that variable, create a message that reveals your favorite number. Print that message.
+// Task:7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Task:8 You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
 
-// Create a Variable
-let favorite_Number = 10;
-let message = "My favorite Number is:";
-console.log(message,favorite_Number); // Print Message and Number:
+// in Addition:
+console.log(4 + 4); // Print Value
+
+// in sunbtraction:
+console.log(12 - 4); // Print Value
+
+// in multiplication:
+console.log(4 * 2); // Print Value
+
+// in Division:
+console.log(16 / 2); // Print Value
