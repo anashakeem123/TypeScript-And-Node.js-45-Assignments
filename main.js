b8e46892d6a7cff23a6d4cda2e71a2f9f915f266
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:00:20 +0500

    Task : 34

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-// Task:35 Think of at least three different animals that have a common characteristic.
-// Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
-// Modify your program to print a statement about each animal, such as A dog would make a great pet. 
-// Add a line at the end of your program stating what these animals have in common. 
-// You could print a sentence such as Any of these animals would make a great pet!
-// Create a Array:
-var animals = ["Cat", "Lion", "Dog"]; // Create a index array:
-for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
-    var animal = animals_1[_i];
-    console.log(animal); // Print a value
+// Task:34 Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+// create a Array:
+var favourite_Pizza = ["Pepperoni ", "Chicken", "veg"]; // Create a index List:
+for (var _i = 0, favourite_Pizza_1 = favourite_Pizza; _i < favourite_Pizza_1.length; _i++) {
+    var pizza = favourite_Pizza_1[_i];
+    console.log(pizza); // Print Message:
 }
-console.log("\n");
-for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
-    var animal = animals_2[_a];
-    console.log(" A ".concat(animal, " has a tail")); // Print a value
+console.log("\n"); // Line space:
+for (var _a = 0, favourite_Pizza_2 = favourite_Pizza; _a < favourite_Pizza_2.length; _a++) {
+    var pizza = favourite_Pizza_2[_a];
+    console.log("I really Like ".concat(pizza, " pizza!")); // Print Message:
 }
-console.log("\n all of these are great pets! but I Love cat more"); // Print Message
+console.log("\nI really Love pizza"); // Print Message:
--- main.ts
+++ main.ts
@@ -1,19 +1,15 @@
-// Task:35 Think of at least three different animals that have a common characteristic.
-// Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
-// Modify your program to print a statement about each animal, such as A dog would make a great pet. 
-// Add a line at the end of your program stating what these animals have in common. 
-// You could print a sentence such as Any of these animals would make a great pet!
+// Task:34 Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
  
-// Create a Array:
-let animals : string [] = ["Cat","Lion","Dog"] // Create a index array:
+// create a Array:
+let favourite_Pizza : string [] = ["Pepperoni ","Chicken","veg"] // Create a index List:
 
-for (let animal of animals) {
-    console.log(animal) // Print a value
+for (let pizza of favourite_Pizza) {
+    console.log(pizza) // Print Message:
 }
-console.log("\n")
-
-for (let animal of animals) {
-    console.log(` A ${animal} has a tail`) // Print a value
+console.log("\n") // Line space:
+for (let pizza of favourite_Pizza){
+    console.log(`I really Like ${pizza} pizza!`) // Print Message:
 }
-
-console.log("\n all of these are great pets! but I Love cat more") // Print Message
+console.log("\nI really Love pizza") // Print Message:
