974d44216f764353e17a0a30dbf77f9e5babc0ed
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:17:08 +0500

    Task : 18

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-// Task:19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-// Create a Array list
-var guestList = ["Anas", "Bilal", "Hamza", "Haider"]; // Create a index list:
-guestList.forEach(function (onGuest) { return console.log("Salam ".concat(onGuest, ", would you Like to dinner with me?")); }); // items list:
-var lenghtGuests = guestList.length;
-console.log("We are Inviting total ".concat(lenghtGuests, " guest.")); // Print Message:
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+//Task:18
+// Making a Array of countries and Print its Orginal Order
+var countriesToVisit = ["China", "Denmark", "Brazil", "Argentina"];
+console.log("Orginal Order:", countriesToVisit);
+// Print the Array in Alphabetical Order without modifying the Actual Array List
+console.log("Alphabetical Order:", __spreadArray([], countriesToVisit, true).sort());
+// show that the array is still in its Orginal Order
+console.log("still in Orginal Order:", countriesToVisit);
+// Print the Array is Reverse Order without the Actualy Array List
+console.log("Reverse Order:", __spreadArray([], countriesToVisit, true).reverse());
+// show that the array is still in its Orginal Order
+console.log("still in Orginal Order:", countriesToVisit);
+// We have Changed the Orginal Array Order Now 
+console.log("Orginal Array Reverse:", countriesToVisit.reverse());
+// Print the array to show that it's back to it's orginal order
+console.log("Back to Orginal Order:", countriesToVisit.reverse());
+// Print the array to show that it's order has been changed in Alphabetical order now
+console.log("sorted in Alphabetical Order:", countriesToVisit.sort());
+// We have Changed again the Orginal Array Order Now in reverse order again
+console.log("Orginal Array Reverse Again:", countriesToVisit.reverse());
--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
-// Task:19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-// Create a Array list
-let guestList = ["Anas","Bilal","Hamza","Haider"]; // Create a index list:
-guestList.forEach(onGuest => console.log(`Salam ${onGuest}, would you Like to dinner with me?`)); // items list:
+//Task:18
+// Making a Array of countries and Print its Orginal Order
+let countriesToVisit:string[]=["China","Denmark","Brazil","Argentina"];
+console.log("Orginal Order:", countriesToVisit);
 
-let lenghtGuests:number = guestList.length;
-console.log(`We are Inviting total ${lenghtGuests} guest.`); // Print Message:
+// Print the Array in Alphabetical Order without modifying the Actual Array List
+console.log("Alphabetical Order:",[...countriesToVisit].sort()); 
+
+// show that the array is still in its Orginal Order
+console.log("still in Orginal Order:", countriesToVisit);
+
+// Print the Array is Reverse Order without the Actualy Array List
+console.log("Reverse Order:", [...countriesToVisit].reverse());
+
+// show that the array is still in its Orginal Order
+console.log("still in Orginal Order:", countriesToVisit);
+
+// We have Changed the Orginal Array Order Now 
+console.log("Orginal Array Reverse:", countriesToVisit.reverse());
+
+// Print the array to show that it's back to it's orginal order
+console.log("Back to Orginal Order:", countriesToVisit.reverse());
+
+// Print the array to show that it's order has been changed in Alphabetical order now
+console.log("sorted in Alphabetical Order:", countriesToVisit.sort());
+
+// We have Changed again the Orginal Array Order Now in reverse order again
+console.log("Orginal Array Reverse Again:", countriesToVisit.reverse());
