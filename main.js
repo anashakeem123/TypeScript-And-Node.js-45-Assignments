4f81ffae5fdb0987d62163f525b94fe1b88c06a6
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:22:23 +0500

    Task : 12

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// Task:13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
-// Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-// Create a Variable:
-var transportationmodes = ["Bus", "car", "bike", "sport car"]; // Create a index value:
-transportationmodes.forEach(function (Mode) { return console.log("I would like to own a ".concat(Mode)); }); // Print List items:
+// Task:12 Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.
+// Create a Variable
+var friendsName = ["Hamza", "Usman", "Haider", "Bilal"]; // Create a index Value
+friendsName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ",How are you?")); }); // Printe List iterm
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-// Task:13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
-// Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+// Task:12 Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.
 
-// Create a Variable:
-let transportationmodes = ["Bus","car","bike","sport car"]; // Create a index value:
-transportationmodes.forEach(Mode => console.log(`I would like to own a ${Mode}`)); // Print List items:
+// Create a Variable
+let friendsName = ["Hamza","Usman","Haider","Bilal"]; // Create a index Value
+
+friendsName.forEach(friendName => console.log(`Hello ${friendName},How are you?`)); // Printe List iterm
