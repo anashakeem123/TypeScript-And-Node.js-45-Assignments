1b51995981dfe40b4b9df1db145d146fed10440d
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:28:05 +0500

    Task : 06

--- main.ts
+++ main.ts
@@ -1,16 +1,10 @@
-// Task:7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-// Task:8 You should create four lines that look like this:
-// console.log(5 + 3)
-// Your output should simply be four lines with the number 8 appearing once on each line.
+// Task:6 Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+// Make sure you use each character combination, "\t" and "\n", at least once. Print the name once,
+//  so the whitespace around the name is displayed. Then print the name after striping the white spaces.
 
-// in Addition:
-console.log(4 + 4); // Print Value
+// Create a Variable
+let white_space_Name = "\n\t Anas Hakeem\n\t";
+console.log(white_space_Name); // Print Message:
 
-// in sunbtraction:
-console.log(12 - 4); // Print Value
-
-// in multiplication:
-console.log(4 * 2); // Print Value
-
-// in Division:
-console.log(16 / 2); // Print Value
+let without_whitespace_Name = white_space_Name.trim(); // Space Remove:
+console.log(without_whitespace_Name); // Print Message:
