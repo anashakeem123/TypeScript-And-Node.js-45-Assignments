b872251705d8225530e11c3842477dcf5d8c997a
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 00:58:32 +0500

    Task : 36

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript.
-// Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+// Task:36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
 function makeShirt(size, text) {
-    if (size === void 0) { size = 'large'; }
-    if (text === void 0) { text = 'I love typeScript'; }
-    console.log("you have order a ".concat(size, ", shirt that says ").concat(text));
+    console.log("you order a ".concat(size, " Shirt that says ").concat(text));
 }
-makeShirt();
-makeShirt('medium');
-//  different Message
-console.log('small', 'I need a big shirt to wear'); // Print Message
+makeShirt('large', '"i love typeScript"'); // Print Large:
+makeShirt('medium', '"i need a big shirt"'); // Print Medium:
--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
-// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript.
-// Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+// Task:36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
 
-function makeShirt(size: string = 'large', text: string = 'I love typeScript'):void {
-    console.log(`you have order a ${size}, shirt that says ${text}`)
-}
-
- makeShirt();
- makeShirt('medium')
-
-//  different Message
-console.log('small', 'I need a big shirt to wear') // Print Message
+function makeShirt(size: string, text: string): void {
+    console.log(`you order a ${size} Shirt that says ${text}`)
+} 
+makeShirt('large','"i love typeScript"') // Print Large:
+makeShirt('medium','"i need a big shirt"') // Print Medium:
