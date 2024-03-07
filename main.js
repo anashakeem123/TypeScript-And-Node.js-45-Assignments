f86995f47bb5d939910d010fa8fa7653c425716f
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:09:30 +0500

    Task : 26

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-// Task:27 Turn your if-else chain from Exercise 5-4 into an if-else chain.
-// If the alien is green, print a message that the player earned 5 points.
-// If the alien is yellow, print a message that the player earned 10 points.
-// If the alien is red, print a message that the player earned 15 points.
-// Write three versions of this program, making sure each message is printed for the appropriate color alien.
-// declear Variable
-var alien_color = "red";
-if (alien_color == "green") {
-    console.log("5 points"); // Print Message
-}
-else if (alien_color == "yellow") {
-    console.log("10 points"); // Print Message
+// Task:26 Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+// If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+// • Write one version of this program that runs the if block and another that runs the else block.
+// Create a Variale:
+var aliens_color = "green";
+if (aliens_color == "green") {
+    console.log("player just earned 5 points for shooting the alien."); // Print Message:
 }
 else {
-    console.log("15 points"); // Print Message
+    console.log("player just earned 10 points."); // Print Message:
+}
+// Declear a Variable:
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("player just earned 5 points for shooting the alien."); // Print Message:
+}
+else {
+    console.log("player just earned 10 points."); // Print Message:
 }
