d40bfe33af330e8c513e8c4a935a4dae86c506cc
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 00:51:04 +0500

    Task : 44

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-// Task:45 Cars: Write a function that stores information about a car in a Object. 
-// The function should always receive a manufacturer and a model name. 
-// It should then accept an arbitrary number of keyword arguments.
-// Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
-// Print the Object that’s returned to make sure all the information was stored correctly.
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-function createCar(manufacture, model, optional) {
-    return __assign({ manufacture: manufacture, model: model }, optional);
+// Task:44 Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
+// The function should have one parameter that collects as many items as the function call provides,
+// and it should print a summary of the sandwich that is being ordered. Call the function three times,
+// using a different number of arguments each time.
+function sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log("Sandwich order:");
+    for (var i = 0; i < items.length; i++) {
+        console.log("- ".concat(items[i]));
+    }
 }
-var mycar = createCar("toyota", "corolla", { color: "silwer", year: "2024" }); // Car List
-console.log(mycar); // Print Value
+console.log("enjoy your sandwich Anas Hakeem"); // Print Message
+sandwich('capsicum', 'tomato', 'chicken'); // sandwich list
+sandwich('beef', 'cheese'); // sandwich list 
+sandwich('garlic chicken', 'mayo sauce'); // sandwich list
--- main.ts
+++ main.ts
@@ -1,23 +1,18 @@
-// Task:45 Cars: Write a function that stores information about a car in a Object. 
-// The function should always receive a manufacturer and a model name. 
-// It should then accept an arbitrary number of keyword arguments.
-// Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
-// Print the Object that’s returned to make sure all the information was stored correctly.
+// Task:44 Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
+// The function should have one parameter that collects as many items as the function call provides,
+// and it should print a summary of the sandwich that is being ordered. Call the function three times,
+// using a different number of arguments each time.
 
+function sandwich(...items: string[]): void {
+    console.log("Sandwich order:")
 
-type car = {
-    manufacture: string
-    model: string
-    [key: string]: any;
-}
-
-function createCar(manufacture: string, model: string, optional: Record<string, any>): car {
-    return{
-        manufacture,
-        model,
-        ...optional
+    for (let i = 0; i < items.length; i++) {
+        console.log(`- ${items[i]}`)
     }
 }
 
-const mycar: car = createCar("toyota", "corolla", {color: "silwer" ,year: "2024"}) // Car List
-console.log(mycar) // Print Value
+console.log("enjoy your sandwich Anas Hakeem") // Print Message
+
+sandwich('capsicum' , 'tomato' , 'chicken') // sandwich list
+sandwich('beef' , 'cheese')  // sandwich list 
+sandwich('garlic chicken' , 'mayo sauce')  // sandwich list
