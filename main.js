a24969a95f376cfdd8d8eccda9560e23459f7933
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:05:01 +0500

    Task : 30

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
-// Task:31 Add an if test to Exercise 28 to make sure the list of users is not empty.
-// • If the list is empty, print the message We need to find some users!
-// • Remove all of the usernames from your array, and make sure the correct message is printed.
-// Create a Array:
+/*// Make a array of five or more usernames,
+ including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website.
+ Loop through the array and print a greeting to each user:*/
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+// Create a Array 
 var users = ["Ali", "Khan", "Bilal", "Haider", "admin"]; // Create a index List
-if (users.length == 0) {
-    console.log("We need to find same users!"); // Print Message
-}
-else {
-    for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
-        var user = users_1[_i];
-        if (user === "admin") {
-            console.log("Hello admin, would you like to see a status report?"); // Print Message
-        }
-        else {
-            console.log("Hello ".concat(user, ", thank you for logging in again")); // Print Message
-        }
+for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+    var user = users_1[_i];
+    if (user == "admin") {
+        console.log("Hello admin, would you like to see a status reports?"); // Print Message
     }
-}
-users = [];
-if (users.length === 0) {
-    console.log("We need to find same users!");
+    else {
+        console.log("Hello ".concat(user, ",thank you for logging in again")); // Print Message
+    }
 }
--- main.ts
+++ main.ts
@@ -1,23 +1,17 @@
-// Task:31 Add an if test to Exercise 28 to make sure the list of users is not empty.
-// • If the list is empty, print the message We need to find some users!
-// • Remove all of the usernames from your array, and make sure the correct message is printed.
+/*// Make a array of five or more usernames,
+ including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website.
+ Loop through the array and print a greeting to each user:*/
+ 
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
-// Create a Array:
+// Create a Array 
 let users : string [] = ["Ali","Khan","Bilal", "Haider", "admin"] // Create a index List
 
-if (users.length == 0) {
-  console.log("We need to find same users!") // Print Message
-} else {
-  for (let user of users) {
-    if ( user === "admin") {
-      console.log("Hello admin, would you like to see a status report?") // Print Message
-    } else {
-      console.log(`Hello ${user}, thank you for logging in again`) // Print Message
-    }
+for(let user of users) {
+  if (user == "admin") {
+    console.log("Hello admin, would you like to see a status reports?") // Print Message
+  } else {
+    console.log(`Hello ${user},thank you for logging in again`) // Print Message
   }
-}
-
-users = []
-if (users.length === 0) {
-  console.log("We need to find same users!")
 }
