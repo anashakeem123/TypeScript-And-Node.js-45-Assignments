93b5745fb51eea318d73e355a0c79d013a2ab142
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:13:10 +0500

    Task : 22

--- main.js
+++ main.js
@@ -1,39 +1,9 @@
-// Task:23 Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
-// let car = 'subaru';
-// console.log("Is car == 'subaru'? I predict True.")
-// console.log(car == 'subaru')
-// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
-// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-// Making a Variables
-var four = 4;
-var ten = 10;
-// Test 1:
-console.log("Test 1: Four is equal to 4?");
-console.log(four == 4); // Print Value:
-// Test 2:
-console.log("\nTest 2: Ten is equal ta 10?");
-console.log(ten == 10); // Print Value:
-// Test 3:
-console.log("\nTest 3: four is not Equal to 10?");
-console.log(four != ten); // Print Value:
-// Test 4:
-console.log("\nTest 4: 10 is greater than 4");
-console.log(ten > 4); // Print Value:
-// Test 5: 
-console.log("\nTest 5: 4 is smaller than 10");
-console.log(four < 10); // Print Value:
-// Test 6:
-console.log("\nTest 6: 10 is smaller than 4");
-console.log(ten < four); // Print Value:
-// Test 7:
-console.log("\nTest 7: 4 is Equal to 10");
-console.log(four == ten); // Print Value:
-// Test 8:
-console.log("\nTest 8: 10 is equal to 10");
-console.log(ten != ten); // Print Value:
-// Test 9:
-console.log("\nTest 9: 4 is greater than 10");
-console.log(four > ten); // Print Value:
-// Test 10:
-console.log("\nTest 10: 100 is smaller than 50");
-console.log(100 < 50); // Print a value
+// Task:22 Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
+// Change an index in one of your programs to produce an index error. 
+// Make sure you correct the error before closing the program.
+// Creating a Array
+var errorArray = ["Ali", "Hamza", "Khan", "Kamran"]; // Create a index List:
+// Producing Error ! by Accessing index of Array
+console.log(errorArray[5]); // Print Message:
+// Error Removed
+console.log(errorArray[2]); // Print Message:
