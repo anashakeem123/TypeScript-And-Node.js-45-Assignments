9131b32a956e4e72a60b9bb6cf66bf07f8b9113a
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:07:43 +0500

    Task : 28

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-// Task:29 Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
-// • Make a array of your three favorite fruits and call it favorite_fruits.
-// • Write five if statements. Each should check whether a certain kind of fruit is in your array.
-// . If the fruit is in your array, the if block should print a statement, such as You really like bananas!
-// Create a Array:
-var favourite_fruits = ["kivi", "Orange", "apple", "berry", "peach"]; // Create a index list:
-if (favourite_fruits.includes("kivi")) {
-    console.log("kivi"); // Print Message:
+// Task:28 Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+// • If the person is less than 2 years old, print a message that the person is a baby.
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+// Create a Variable:
+var age = 100;
+if (age < 2) {
+    console.log("you are a baby"); // print this Message:
 }
-if (favourite_fruits.includes("apple")) {
-    console.log("apple"); // Print Message:
+else if (age < 4) {
+    console.log("you are a toddler"); // print this Message:
 }
-if (favourite_fruits.includes("Orange")) {
-    console.log("Orange"); // Print Message:
+else if (age < 13) {
+    console.log("you are a kid"); // print this Message:
 }
-if (favourite_fruits.includes("berry")) {
-    console.log("you really like bananas"); // Print Message:
+else if (age < 20) {
+    console.log("you are a teenager"); // print this Message:
 }
-if (favourite_fruits.includes("peach")) {
-    console.log("you really like bananas"); // Print Message:
+else if (age < 65) {
+    console.log("you are a adult"); // print this Message:
+}
+else {
+    console.log("you are older"); // print this Message:
 }
