0d4561d9921ca50f68daa0f277b22f7c9e7f9ab1
Anas HAkeem <160387360+anashakeem123@users.noreply.github.com>
2024-03-08 01:08:35 +0500

    Task : 27

--- main.ts
+++ main.ts
@@ -1,24 +1,14 @@
-// Task:28 Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
-// • If the person is less than 2 years old, print a message that the person is a baby.
-// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
-// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
-// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
-// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
-// • If the person is age 65 or older, print a message that the person is an elder.
-
-// Create a Variable:
-let age : number = 100;
-
-if (age < 2) {
-    console.log("you are a baby") // print this Message:
-} else if (age < 4) {
-    console.log("you are a toddler") // print this Message:
-} else if (age < 13) {
-    console.log("you are a kid") // print this Message:
-} else if (age < 20) {
-    console.log("you are a teenager") // print this Message:
-} else if (age < 65) {
-    console.log("you are a adult") // print this Message:
+// Task:27 Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// If the alien is green, print a message that the player earned 5 points.
+// If the alien is yellow, print a message that the player earned 10 points.
+// If the alien is red, print a message that the player earned 15 points.
+// Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// declear Variable
+let alien_color: string = "red"
+if (alien_color == "green") {
+console.log("5 points") // Print Message
+} else if (alien_color == "yellow") {
+    console.log("10 points") // Print Message
 } else {
-    console.log("you are older") // print this Message:
+    console.log("15 points") // Print Message
 }
